228b29607c78c29677a29049f0112c00adebcf13
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-04-11 01:44:48 +0300

    add linters and fix linter errors

--- java.js
+++ java.js
@@ -1,29 +1,24 @@
 const hamburger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const mobilelist = document.getElementById('mobilelist');
 const mobilelistlink = document.querySelectorAll('#mobilelist li a');
 const cancelBtn = document.querySelector('#cancel-btn');
 
-console.log(mobileMenu);
-hamburger.addEventListener('click',function() {
-    hamburger.classList.add('hide');
-    cancelBtn.classList.remove('hide');
-    mobileMenu.style.display= 'flex';
-
+hamburger.addEventListener('click', () => {
+  hamburger.classList.add('hide');
+  cancelBtn.classList.remove('hide');
+  mobileMenu.style.display = 'flex';
 });
 
-for (const a of mobilelistlink) {
-    a.addEventListener('click',function() {
-        mobileMenu.style.display= 'none';
-        console.log('link clicked');
-        cancelBtn.classList.add('hide');
-        hamburger.classList.remove('hide');
-    });
-}
-
-cancelBtn.addEventListener('click',function(){
+mobilelistlink.forEach((a) => {
+  a.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
     cancelBtn.classList.add('hide');
     hamburger.classList.remove('hide');
-    mobileMenu.style.display = "none";
-    
+  });
+});
+
+cancelBtn.addEventListener('click', () => {
+  cancelBtn.classList.add('hide');
+  hamburger.classList.remove('hide');
+  mobileMenu.style.display = 'none';
 });
