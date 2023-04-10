6cdbe28019a4a85a4f861f4475221841c651192f
Fezzopro <fezzopro@gmail.com>
2023-04-10 17:04:11 +0200

    Add eventListener to menu links

--- java.js
+++ java.js
@@ -1,19 +1,29 @@
 let btn = document.querySelector('.menu');
 let mobileMenu = document.getElementById('desktop-nav');
 let mobilelist = document.getElementById('mobilelist');
+let mobilelistlink = document.querySelectorAll('#mobilelist li a');
+
 console.log(mobileMenu);
 btn.addEventListener('click',function() {
     mobileMenu.style.display= 'flex';
-    mobilelist.style.display = 'flex';
-    mobilelist.style['flex-direction']= "column";
     mobileMenu.style.justifyContent ="left";
     mobileMenu.style.background = 'white';
     mobileMenu.style.height = '100vh';
     mobileMenu.style.position = 'fixed';
+    mobileMenu.style.order = '1';
     mobileMenu.style.top = 0;
     mobileMenu.style.zIndex='999';
-    // mobileMenu.style.content= 'url(icon-Cancel.png)';
-    
-    
+
+    mobilelist.style.display = 'flex';
+    mobilelist.style['flex-direction']= "column";
+    mobilelist.style['gap']= "12px";
+    mobilelist.style['margin-top']= "20px";
 });
+
+for (const a of mobilelistlink) {
+    a.addEventListener('click',function() {
+        mobileMenu.style.display= 'none';
+        console.log('link clicked');
+    });
+}
 
