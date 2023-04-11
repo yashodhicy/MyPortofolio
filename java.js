25442e18c8c6cb09578556823207eb94c5b5a064
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-04-11 20:35:18 +0300

    add object array and workcard dyanamic code

--- java.js
+++ java.js
@@ -19,3 +19,84 @@ mobilelistlink.forEach((a) => {
 });
 
 cancelBtn.addEventListener('click', hideMobileMenu);
+
+const projectDetails = [
+  {
+    name: "Project 1",
+    description: "Lorem ipsum dolor sit amet",
+    image: "images/popupmobile.png",
+    technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
+    'live version': "https://yashodhicy.github.io/",
+    source: "https://github.com/yashodhicy/MyPortofolio",
+  },
+  {
+    name: "Project 2",
+    description: "Lorem ipsum dolor sit amet",
+    image: "images/popupmobile.png",
+    technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
+    'live version': "https://yashodhicy.github.io/",
+    source: "https://github.com/yashodhicy/MyPortofolio",
+  },
+  {
+    name: "Project 3",
+    description: "Lorem ipsum dolor sit amet",
+    image: "images/popupmobile.png",
+    technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
+    live: "https://yashodhicy.github.io/",
+    source: "https://github.com/yashodhicy/MyPortofolio",
+  },
+]
+
+const projectContainer = document.getElementById("projects");
+
+// const fragment = new DocumentFragment();
+
+projectDetails.forEach((project) => {
+  const projectDiv = document.createElement('div');
+  projectDiv.classList.add("project");
+  projectDiv.classList.add('work-card');
+
+  const titleEl = document.createElement("h3");
+  titleEl.innerText = project.name;
+  projectDiv.appendChild(titleEl);
+  // titleEl.classList.add('');
+
+  const descriptionEl = document.createElement("p");
+  descriptionEl.innerText = project.description;
+  projectDiv.appendChild(descriptionEl);
+
+  const imageEl = document.createElement("img");
+  imageEl.src = project.image;
+  projectDiv.appendChild(imageEl);
+
+  const technologiesEl = document.createElement("ul");
+  project.technologies.forEach((technology) => {
+    const technologyEl = document.createElement("li");
+    technologyEl.innerText = technology;
+    technologiesEl.appendChild(technologyEl);
+  });
+  projectDiv.appendChild(technologiesEl);
+
+  const liveDemo = document.createElement("a");
+  liveDemo.href = project.live;
+  projectDiv.appendChild(liveDemo);
+
+  const sourceEl = document.createElement("a");
+  sourceEl.href = project.source;
+  projectDiv.appendChild(sourceEl);
+  
+  projectContainer.appendChild(projectDiv);
+});
+
+const preview = document.querySelector('.see_project');
+const popup = document.querySelector('#popup')
+
+const projectdetails = () => {
+  preview.classList.toggle('hide');
+  popup.style.display = popup.style.display === 'flex' ? 'none' : 'flex';
+}
+
+preview.addEventListener('click',projectDetails);
+
+
+
