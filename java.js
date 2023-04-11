03e5e0f9b8d06b5bdad5d1b5a4d4229f3f418477
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-04-11 10:49:24 +0300

    update style file to look simillar to figma

--- java.js
+++ java.js
@@ -4,21 +4,18 @@ const mobilelistlink = document.querySelectorAll('#mobilelist li a');
 const cancelBtn = document.querySelector('#cancel-btn');
 
 const toggleMobileMenu = () => {
- hamburger.classList.toggle('hide');
- cancelBtn.classList.toggle('hide');
- mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
-}
-
+  hamburger.classList.toggle('hide');
+  cancelBtn.classList.toggle('hide');
+  mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+};
 const hideMobileMenu = () => {
- hamburger.classList.remove('hide');
- cancelBtn.classList.add('hide');
- mobileMenu.style.display = 'none';
-}
-
+  hamburger.classList.remove('hide');
+  cancelBtn.classList.add('hide');
+  mobileMenu.style.display = 'none';
+};
 hamburger.addEventListener('click', toggleMobileMenu);
-
 mobilelistlink.forEach((a) => {
- a.addEventListener('click', hideMobileMenu);
+  a.addEventListener('click', hideMobileMenu);
 });
 
 cancelBtn.addEventListener('click', hideMobileMenu);
