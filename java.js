488702d7c8d3d1089ff99edbf743a69d710a0ffa
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-04-12 21:58:13 +0300

    add popup window mobile and desktop view

--- java.js
+++ java.js
@@ -20,101 +20,159 @@ mobilelistlink.forEach((a) => {
 
 cancelBtn.addEventListener('click', hideMobileMenu);
 
+// for creating a list of buttons for workcard and popup
+const createButtonList = (project, techlist) => {
+  project.technologies.forEach((technology) => {
+    const technologyEl = document.createElement('li');
+    const techbutton = document.createElement('button');
+    techbutton.innerText = technology;
+    technologyEl.appendChild(techbutton);
+    techlist.appendChild(technologyEl);
+    technologyEl.classList.add('tags');
+  });
+};
+
+// Create project details object array
+
 const projectDetails = [
   {
-    name: "Project 1",
-    description: "Lorem ipsum dolor sit amet",
-    image: "images/popupmobile.png",
+    name: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    image: 'images/popupmobile.png',
     technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
-    'live version': "https://yashodhicy.github.io/",
-    source: "https://github.com/yashodhicy/MyPortofolio",
+    live: 'https://yashodhicy.github.io/',
+    source: 'https://github.com/yashodhicy/MyPortofolio',
   },
   {
-    name: "Project 2",
-    description: "Lorem ipsum dolor sit amet",
-    image: "images/popupmobile.png",
+    name: 'Project 2',
+    description: 'Lorem ipsum dolor sit amet',
+    image: 'images/popupmobile.png',
     technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
-    'live version': "https://yashodhicy.github.io/",
-    source: "https://github.com/yashodhicy/MyPortofolio",
+    live: 'https://yashodhicy.github.io/',
+    source: 'https://github.com/yashodhicy/MyPortofolio',
   },
   {
-    name: "Project 3",
-    description: "Lorem ipsum dolor sit amet",
-    image: "images/popupmobile.png",
+    name: 'Project 3',
+    description: 'Lorem ipsum dolor sit amet',
+    image: 'images/popupmobile.png',
     technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
-    live: "https://yashodhicy.github.io/",
-    source: "https://github.com/yashodhicy/MyPortofolio",
+    live: 'https://yashodhicy.github.io/',
+    source: 'https://github.com/yashodhicy/MyPortofolio',
   },
-]
 
-const projectContainer = document.getElementById("projects");
+  {
+    name: 'Project 4',
+    description: 'Lorem ipsum dolor sit amet',
+    image: 'images/popupmobile.png',
+    technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
+    live: 'https://yashodhicy.github.io/',
+    source: 'https://github.com/yashodhicy/MyPortofolio',
+  },
 
-// const fragment = new DocumentFragment();
+  {
+    name: 'Project 5',
+    description: 'Lorem ipsum dolor sit amet',
+    image: 'images/popupmobile.png',
+    technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
+    live: 'https://yashodhicy.github.io/',
+    source: 'https://github.com/yashodhicy/MyPortofolio',
+  },
+
+  {
+    name: 'Project 6',
+    description: 'Lorem ipsum dolor sit amet',
+    image: 'images/popupmobile.png',
+    technologies: ['Ruby on rails', 'HTML', 'CSS', 'JavaScript'],
+    live: 'https://yashodhicy.github.io/',
+    source: 'https://github.com/yashodhicy/MyPortofolio',
+  },
+];
+
+// implement popup window
+
+const popup = document.querySelector('#popup');
+const cancel = document.querySelector('#cancel-btn2');
+const main = document.querySelector('main');
+
+const projectdetailspopup = (project) => {
+  popup.appendChild(cancel);
+
+  popup.classList.toggle('hide');
+  main.classList.add('display');
+
+  const popupImage = document.querySelector('#popup-image');
+  const popupTitle = document.querySelector('#popup-title');
+  const popupTechs = document.querySelector('#popup-techs');
+  const popupDescription = document.querySelector('#popup-description');
+  const popuplive = document.querySelector('#popup-livelink');
+  const popupsource = document.querySelector('#popup-source');
+  popupTechs.textContent = '';
+
+  popupImage.src = project.image;
+  popupTitle.textContent = project.name;
+
+  const liveDemo = document.createElement('a');
+  liveDemo.href = project.live;
+  popuplive.appendChild(liveDemo);
+
+  const sourceEl = document.createElement('a');
+  sourceEl.href = project.source;
+  popupsource.appendChild(sourceEl);
+
+  createButtonList(project, popupTechs);
+  popupDescription.innerText = project.description;
+
+  popup.style.display = 'block';
+};
+
+// create work section
+const workEL = document.createElement('section');
+workEL.classList.add('work');
+
+const fragment = document.createDocumentFragment();
 
 projectDetails.forEach((project) => {
   const projectDiv = document.createElement('div');
-  projectDiv.classList.add("project");
   projectDiv.classList.add('work-card');
 
-  const titleEl = document.createElement("h3");
+  const textEl = document.createElement('div');
+  textEl.classList.add('text');
+  projectDiv.appendChild(textEl);
+
+  const titleEl = document.createElement('h3');
   titleEl.innerText = project.name;
-  projectDiv.appendChild(titleEl);
-  // titleEl.classList.add('');
+  textEl.appendChild(titleEl);
 
-  const descriptionEl = document.createElement("p");
-  descriptionEl.innerText = project.description;
-  projectDiv.appendChild(descriptionEl);
+  const technologiesEl = document.createElement('ul');
+  createButtonList(project, technologiesEl);
 
-  const imageEl = document.createElement("img");
-  imageEl.src = project.image;
-  projectDiv.appendChild(imageEl);
+  textEl.appendChild(technologiesEl);
 
-  const technologiesEl = document.createElement("ul");
-  project.technologies.forEach((technology) => {
-    const technologyEl = document.createElement("li");
-    technologyEl.innerText = technology;
-    technologiesEl.appendChild(technologyEl);
+  const seeproject = document.createElement('button');
+  seeproject.innerText = 'see Project';
+  seeproject.classList.add('see_project');
+  // seeproject.setAttribute('id', `project-${index}`);
+  textEl.appendChild(seeproject);
+
+  seeproject.addEventListener('click', () => {
+    projectdetailspopup(project);
   });
-  projectDiv.appendChild(technologiesEl);
 
-  const liveDemo = document.createElement("a");
-  liveDemo.href = project.live;
-  projectDiv.appendChild(liveDemo);
-
-  const sourceEl = document.createElement("a");
-  sourceEl.href = project.source;
-  projectDiv.appendChild(sourceEl);
-  
-  projectContainer.appendChild(projectDiv);
+  fragment.appendChild(projectDiv);
 });
 
-const preview = document.querySelector('.see_project');
+// projectContainer.appendChild(fragment);
 
-const popup = document.querySelector('#popup')
+workEL.appendChild(fragment);
+document.body.appendChild(workEL);
 
-const projectdetails = () => {
-    
-    popup.classList.toggle('hide');
+// add this section after topic div
+const divtopic = document.querySelector('#topic');
 
-    const popupImage = document.querySelector('#popup-image');
-    const popupTitle = document.querySelector('#popup-title');
-    const popupTechs = document.querySelector('#popup-techs');
-    const popupDescription = document.querySelector('#popup-description');
-    popupTechs.textContent='';
+// add workEL as the second section below the div container
+divtopic.insertAdjacentElement('afterend', workEL);
 
-    popupImage.src = projectDetails[0].image;
-    popupTitle.textContent = projectDetails[0].name;
-
-    projectDetails[0].technologies.forEach((tech) => {
-        const techList = document.createElement('li');
-        techList.textContent = tech;
-        popupTechs.appendChild(techList);
-    });
-    
-    popupDescription.textContent = projectDetails[0].description;
-};
-
-preview.addEventListener('click', projectdetails);
-
-
-
+cancel.addEventListener('click', () => {
+  popup.classList.toggle('hide');
+  main.classList.remove('display');
+});
